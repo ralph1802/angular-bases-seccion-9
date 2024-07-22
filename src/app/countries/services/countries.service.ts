6e17cc75218499799b977fb2f518a5e81e5513d9
Axel Leos <rleos1802@outlook.com>
2024-07-22 12:14:04 -0600

    Fin de la seccion 9

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, count, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,6 +10,17 @@ export class CountriesService {
 
   constructor(private http: HttpClient) { }
 
+
+  searchCountryByAlphaCode( code: string): Observable<Country | null> {
+
+    const url = `${ this.apiUrl }/alpha/${ code }`;
+
+    return this.http.get<Country[]>( url )
+    .pipe(
+      map( countries => countries.length > 0 ? countries[0]  : null ),
+      catchError( () =>  of(null) )
+    );
+  }
 
   searchCapital(term: string): Observable<Country[]> {
 
