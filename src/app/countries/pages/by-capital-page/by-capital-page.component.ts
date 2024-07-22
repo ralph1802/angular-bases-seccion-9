304b8ae7d0ab72d3c8e06133c697b3a35e979870
Axel Leos <rleos1802@outlook.com>
2024-07-22 08:47:43 -0600

    128.Argumentos por URL - Página de país

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -21,8 +21,4 @@ export class ByCapitalPageComponent {
     });
 
   }
-
-
-
-
 }
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -13,18 +13,30 @@ export class CountriesService {
 
   searchCapital(term: string): Observable<Country[]> {
 
-    const url = `${ this.apiUrl }/capital/${ term }`
-
+    const url = `${ this.apiUrl }/capital/${ term }`;
     return this.http.get<Country[]>( url )
     .pipe(
       catchError( () =>  of([]) )
     );
   }
 
+  searchCountry(term: string): Observable<Country[]> {
 
+    const url = `${ this.apiUrl }/name/${ term }`;
+    return this.http.get<Country[]>( url )
+    .pipe(
+      catchError( () =>  of([]) )
+    );
+  }
 
+  searchRegion(region: string): Observable<Country[]> {
 
-
+    const url = `${ this.apiUrl }/region/${ region}`;
+    return this.http.get<Country[]>( url )
+    .pipe(
+      catchError( () =>  of([]) )
+    );
+  }
 
 
 }
